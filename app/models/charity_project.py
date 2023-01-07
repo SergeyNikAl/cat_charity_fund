15c2929a181e0d7ae5e847ba4e20651746889926
SergeyNikAl <cergei_nikulin@mail.ru>
2023-01-07 20:32:40 +0300

    fix all

--- app/models/charity_project.py
+++ app/models/charity_project.py
@@ -3,8 +3,6 @@ from sqlalchemy import Column, String, Text
 from .base import CharityDonationBase
 
 
-
-
 class CharityProject(CharityDonationBase):
 
     name = Column(String(100), unique=True, nullable=False)
